196c31f2f91e04b49f8f24708b66c7257f7ba775
Shirin Sinha <150991632+shirinsinha23@users.noreply.github.com>
2026-09-17 22:17:18 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (28.54%) - LeetSync

---
+++ 492-construct-the-rectangle/construct-the-rectangle.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] constructRectangle(int area) {
+        int W = (int) Math.sqrt(area);
+        
+        while (area % W != 0) {
+            W--;
+        }
+        
+        int L = area / W;
+        
+        return new int[]{L, W};
+    }
+}
